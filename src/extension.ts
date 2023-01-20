f72ba520f11e3e639d1c66f5feac2acfdfe60ad8
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2023-01-20 16:22:58 +0100

    Changed to const and added two commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { TextEncoder } from "util";
 import * as vscode from "vscode";
 import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, "t3-cua-tools" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  const disposableScreen = vscode.commands.registerCommand(
     "t3-cua-tools.newScreen",
     async () => {
-      // The code you place here will be executed every time your command is executed
-
-      let screenName = await vscode.window.showInputBox({
-        placeHolder: "new-screen",
+      const screenName = await vscode.window.showInputBox({
+        placeHolder: "NewScreen",
         prompt: "Enter the name of the new screen",
       });
       // Check if the user input is not empty
@@ -33,14 +20,17 @@ export function activate(context: vscode.ExtensionContext) {
       }
 
       // Get the active workspace folder
-      let workspaceFolder = vscode.workspace.workspaceFolders
+      const workspaceFolder = vscode.workspace.workspaceFolders
         ? vscode.workspace.workspaceFolders[0]
         : undefined;
       if (!workspaceFolder) {
-        return; // No open workspace
+        vscode.window.showInformationMessage(
+          "No workspace folder is open. Please open a workspace folder and try again."
+        );
+        return;
       }
 
-      let folderUri = vscode.Uri.file(
+      const folderUri = vscode.Uri.file(
         path.join(
           workspaceFolder.uri.fsPath,
           "packages",
@@ -49,19 +39,21 @@ export function activate(context: vscode.ExtensionContext) {
           screenName
         )
       );
-      let fileUri = vscode.Uri.file(path.join(folderUri.fsPath, "screen.tsx"));
+      const fileUri = vscode.Uri.file(
+        path.join(folderUri.fsPath, "screen.tsx")
+      );
 
-      // Create the new folder
+      // Create the new screen folder
       vscode.workspace.fs.createDirectory(folderUri);
 
-      // Create the new file
-      let screenFileData = new TextEncoder().encode(
+      // Create the new screen file
+      const screenFileData = new TextEncoder().encode(
         `import { Button, Paragraph, YStack } from "@my/ui";\nimport { ChevronLeft } from "@tamagui/lucide-icons";\nimport React from "react";\nimport { useLink } from "solito/link";\n\nexport function ${screenName}() {\n  const linkProps = useLink({ href: "/" });\n\n  return (\n    <YStack f={1} jc="center" ai="center" space>\n      <Paragraph ta="center" fow="800">\n        ${screenName.toLowerCase()}\n      </Paragraph>\n      <Button {...linkProps} icon={ChevronLeft} theme="gray">\n        Go Home\n      </Button>\n    </YStack>\n  );\n} `
       );
       await vscode.workspace.fs.writeFile(fileUri, screenFileData);
 
-      // For navigation/native/index.tsx
-      let navigationFileUri = vscode.Uri.file(
+      // Same for navigation/native/index.tsx
+      const navigationFileUri = vscode.Uri.file(
         path.join(
           workspaceFolder.uri.fsPath,
           "packages",
@@ -72,20 +64,19 @@ export function activate(context: vscode.ExtensionContext) {
         )
       );
 
-      // Open the text document
-      let navigationIndex = await vscode.workspace.openTextDocument(
+      const navigationIndex = await vscode.workspace.openTextDocument(
         navigationFileUri
       );
-      let navContents = navigationIndex.getText();
+      const navContents = navigationIndex.getText();
 
-      let newImport = `import { ${screenName} } from "../../features/${screenName.toLowerCase()}/screen";\n`;
-      let startingCreateStack = navContents.lastIndexOf("import");
-      let newRoute = `  ${screenName.toLowerCase()}: undefined;\n`;
-      let closingCreateStack = navContents.indexOf("}>();");
-      let newStackScreen = `  <Stack.Screen\n        name="${screenName.toLowerCase()}"\n        component={${screenName}}\n        options={{\n          title: "${screenName}",\n        }}\n      />\n`;
-      let closingNavIndex = navContents.indexOf("</Stack.Navigator>");
+      const newImport = `import { ${screenName} } from "../../features/${screenName.toLowerCase()}/screen";\n`;
+      const startingCreateStack = navContents.lastIndexOf("import");
+      const newRoute = `  ${screenName.toLowerCase()}: undefined;\n`;
+      const closingCreateStack = navContents.indexOf("}>();");
+      const newStackScreen = `  <Stack.Screen\n        name="${screenName.toLowerCase()}"\n        component={${screenName}}\n        options={{\n          title: "${screenName}",\n        }}\n      />\n`;
+      const closingNavIndex = navContents.indexOf("</Stack.Navigator>");
 
-      let newNavContents =
+      const newNavContents =
         navContents.substring(0, startingCreateStack) +
         newImport +
         navContents.substring(startingCreateStack, closingCreateStack) +
@@ -95,11 +86,11 @@ export function activate(context: vscode.ExtensionContext) {
         navContents.substring(closingNavIndex);
 
       // Write the new contents to the navigation file
-      let newNavFileData = new TextEncoder().encode(newNavContents);
+      const newNavFileData = new TextEncoder().encode(newNavContents);
       vscode.workspace.fs.writeFile(navigationFileUri, newNavFileData);
 
-      // For provider/navigation/index.tsx
-      let navigationProviderFileUri = vscode.Uri.file(
+      // Same for provider/navigation/index.tsx
+      const navigationProviderFileUri = vscode.Uri.file(
         path.join(
           workspaceFolder.uri.fsPath,
           "packages",
@@ -110,24 +101,24 @@ export function activate(context: vscode.ExtensionContext) {
         )
       );
 
-      let navigationProviderIndex = await vscode.workspace.openTextDocument(
+      const navigationProviderIndex = await vscode.workspace.openTextDocument(
         navigationProviderFileUri
       );
-      let navProviderContents = navigationProviderIndex.getText();
+      const navProviderContents = navigationProviderIndex.getText();
 
-      let newNavRoute = `\n              ${screenName.toLowerCase()}: "${screenName.toLowerCase()}",\n`;
-      let closingScreenObject = navProviderContents.indexOf(
+      const newNavRoute = `  ${screenName.toLowerCase()}: "${screenName.toLowerCase()}",\n`;
+      const closingScreenObject = navProviderContents.indexOf(
         "},",
         navProviderContents.indexOf("screens: {")
       );
 
-      let newNavProviderContents =
+      const newNavProviderContents =
         navProviderContents.substring(0, closingScreenObject) +
         newNavRoute +
         navProviderContents.substring(closingScreenObject);
 
       // Write the new contents to the navigation file
-      let newNavProviderFileData = new TextEncoder().encode(
+      const newNavProviderFileData = new TextEncoder().encode(
         newNavProviderContents
       );
       vscode.workspace.fs.writeFile(
@@ -135,8 +126,8 @@ export function activate(context: vscode.ExtensionContext) {
         newNavProviderFileData
       );
 
-      // For apps/nextjs/pages
-      let nextFolderUri = vscode.Uri.file(
+      // Same for apps/nextjs/pages
+      const nextFolderUri = vscode.Uri.file(
         path.join(
           workspaceFolder.uri.fsPath,
           "apps",
@@ -145,28 +136,172 @@ export function activate(context: vscode.ExtensionContext) {
           screenName
         )
       );
-      let nextFileUri = vscode.Uri.file(
+      const nextFileUri = vscode.Uri.file(
         path.join(nextFolderUri.fsPath, "index.tsx")
       );
 
-      // Create the new folder
+      // Create the new nextjs folder
       vscode.workspace.fs.createDirectory(nextFolderUri);
 
-      // Create the new file
-      let nextFileData = new TextEncoder().encode(
+      // Create the new nextjs file
+      const nextFileData = new TextEncoder().encode(
         `import { ${screenName} } from 'app/features/${screenName.toLowerCase()}/screen'\n\nexport default ${screenName}\n`
       );
       await vscode.workspace.fs.writeFile(nextFileUri, nextFileData);
 
-      // Open the screen.tsx
-      let newScreenDocument = await vscode.workspace.openTextDocument(fileUri);
+      // Open the screen.tsx file
+      const newScreenDocument = await vscode.workspace.openTextDocument(
+        fileUri
+      );
 
       // Show it in the editor
       vscode.window.showTextDocument(newScreenDocument);
     }
   );
 
-  context.subscriptions.push(disposable);
+  const disposableComponent = vscode.commands.registerCommand(
+    "t3-cua-tools.newComponent",
+    async () => {
+      const componentName = await vscode.window.showInputBox({
+        placeHolder: "NewComponent",
+        prompt: "Enter the name of the new component",
+      });
+      // Check if the user input is not empty
+      if (!componentName || componentName.includes(" ")) {
+        // Do something with the user input
+        vscode.window.showInformationMessage(
+          `Enter a valid component name. You entered: ${componentName}`
+        );
+        return;
+      }
+
+      // Get the active workspace folder
+      const workspaceFolder = vscode.workspace.workspaceFolders
+        ? vscode.workspace.workspaceFolders[0]
+        : undefined;
+      if (!workspaceFolder) {
+        vscode.window.showInformationMessage(
+          "No workspace folder is open. Please open a workspace folder and try again."
+        );
+        return;
+      }
+
+      const newFileUri = vscode.Uri.file(
+        path.join(
+          workspaceFolder.uri.fsPath,
+          "packages",
+          "ui",
+          "src",
+          "components",
+          componentName + ".tsx"
+        )
+      );
+
+      // Create the new component file
+      const componentFileData = new TextEncoder().encode(
+        `import { Paragraph, YStack } from "@my/ui";\nimport React from "react";\nimport { useLink } from "solito/link";\n\nexport function ${componentName}() {\n  return (\n    <YStack f={1} jc="center" ai="center" space>\n      <Paragraph ta="center" fow="800">\n        ${componentName}\n      </Paragraph>\n    </YStack>\n  );\n} `
+      );
+      await vscode.workspace.fs.writeFile(newFileUri, componentFileData);
+
+      // Open the {componentName}.tsx file
+      const newComponentDocument = await vscode.workspace.openTextDocument(
+        newFileUri
+      );
+
+      // Show it in the editor
+      vscode.window.showTextDocument(newComponentDocument);
+    }
+  );
+
+  const disposableRoute = vscode.commands.registerCommand(
+    "t3-cua-tools.newRoute",
+    async () => {
+      const routeName = await vscode.window.showInputBox({
+        placeHolder: "newRoute",
+        prompt: "Enter the name of the new route",
+      });
+      // Check if the user input is not empty
+      if (!routeName || routeName.includes(" ")) {
+        // Do something with the user input
+        vscode.window.showInformationMessage(
+          `Enter a valid route name. You entered: ${routeName}`
+        );
+        return;
+      }
+
+      // Get the active workspace folder
+      const workspaceFolder = vscode.workspace.workspaceFolders
+        ? vscode.workspace.workspaceFolders[0]
+        : undefined;
+      if (!workspaceFolder) {
+        vscode.window.showInformationMessage(
+          "No workspace folder is open. Please open a workspace folder and try again."
+        );
+        return;
+      }
+
+      const newFileUri = vscode.Uri.file(
+        path.join(
+          workspaceFolder.uri.fsPath,
+          "packages",
+          "api",
+          "src",
+          "router",
+          routeName + ".ts"
+        )
+      );
+
+      // Create the new route file
+      const routeFileData = new TextEncoder().encode(
+        `import { protectedProcedure, publicProcedure, router } from "../trpc";\n\nexport const ${routeName}Router = router({\n\n});\n`
+      );
+      await vscode.workspace.fs.writeFile(newFileUri, routeFileData);
+
+      const routerIndexFileUri = vscode.Uri.file(
+        path.join(
+          workspaceFolder.uri.fsPath,
+          "packages",
+          "api",
+          "src",
+          "router",
+          "index.ts"
+        )
+      );
+
+      const navigationProviderIndex = await vscode.workspace.openTextDocument(
+        routerIndexFileUri
+      );
+      const RouterContents = navigationProviderIndex.getText();
+
+      const newImport = `import { ${routeName}Router } from "./${routeName}";\n`;
+      const lastImportIndex = RouterContents.lastIndexOf("import");
+      const newRoute = `  ${routeName}: ${routeName}Router,\n`;
+      const closingRouterObject = RouterContents.indexOf("});");
+
+      const newRouterContents =
+        RouterContents.substring(0, lastImportIndex) +
+        newImport +
+        RouterContents.substring(lastImportIndex, closingRouterObject) +
+        newRoute +
+        RouterContents.substring(closingRouterObject);
+
+      // Write the new contents to the navigation file
+      const newRouterFileData = new TextEncoder().encode(newRouterContents);
+      vscode.workspace.fs.writeFile(routerIndexFileUri, newRouterFileData);
+
+      // Open the {componentName}.tsx file
+      const newRouteDocument = await vscode.workspace.openTextDocument(
+        newFileUri
+      );
+
+      // Show it in the editor
+      vscode.window.showTextDocument(newRouteDocument);
+    }
+  );
+
+  context.subscriptions.push(disposableScreen);
+  context.subscriptions.push(disposableComponent);
+  context.subscriptions.push(disposableRoute);
 }
 
 // this method is called when your extension is deactivated
