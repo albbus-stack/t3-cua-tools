a07287b9dad4802ff5b1ffc15f765194a648ba2f
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2023-01-22 16:03:44 +0100

    Supports the expo router template & dynamic routes

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,30 @@ import { TextEncoder } from "util";
 import * as vscode from "vscode";
 import * as path from "path";
 
+const isWithExpoRouter = async (workspaceFolder: vscode.WorkspaceFolder) => {
+  let folderUri = vscode.Uri.file(
+    path.join(
+      workspaceFolder.uri.fsPath,
+      "packages",
+      "app",
+      "navigation",
+      "native"
+    )
+  );
+
+  let withExpoRouter = true;
+
+  await vscode.workspace.fs.stat(folderUri).then((stat) => {
+    if (stat.type === vscode.FileType.Directory) {
+      withExpoRouter = false;
+    }
+  });
+
+  console.log("wep:", withExpoRouter);
+
+  return withExpoRouter;
+};
+
 export function activate(context: vscode.ExtensionContext) {
   const disposableScreen = vscode.commands.registerCommand(
     "t3-cua-tools.newScreen",
@@ -12,11 +36,31 @@ export function activate(context: vscode.ExtensionContext) {
       });
       // Check if the user input is not empty
       if (!screenName || screenName.includes(" ")) {
-        // Do something with the user input
         vscode.window.showInformationMessage(
           `Enter a valid screen name. You entered: ${screenName}`
         );
         return;
+      }
+
+      const isStaticRoute = await vscode.window
+        .showQuickPick(["Static Route", "Dynamic Route"], {
+          placeHolder: "What type of route does this screen depend on?",
+        })
+        .then((result) => result === "Static Route");
+
+      let parameterName = undefined;
+      if (!isStaticRoute) {
+        parameterName = await vscode.window.showInputBox({
+          placeHolder: "id",
+          prompt: "Enter the name of the dynamic route parameter",
+        });
+        // Check if the user input is not empty
+        if (!parameterName || parameterName.includes(" ")) {
+          vscode.window.showInformationMessage(
+            `Enter a valid parameter name. You entered: ${parameterName}`
+          );
+          return;
+        }
       }
 
       // Get the active workspace folder
@@ -36,7 +80,7 @@ export function activate(context: vscode.ExtensionContext) {
           "packages",
           "app",
           "features",
-          screenName
+          screenName.toLowerCase()
         )
       );
       const fileUri = vscode.Uri.file(
@@ -47,85 +91,150 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.workspace.fs.createDirectory(folderUri);
 
       // Create the new screen file
-      const screenFileData = new TextEncoder().encode(
-        `import { Button, Paragraph, YStack } from "@my/ui";\nimport { ChevronLeft } from "@tamagui/lucide-icons";\nimport React from "react";\nimport { useLink } from "solito/link";\n\nexport function ${screenName}() {\n  const linkProps = useLink({ href: "/" });\n\n  return (\n    <YStack f={1} jc="center" ai="center" space>\n      <Paragraph ta="center" fow="800">\n        ${screenName.toLowerCase()}\n      </Paragraph>\n      <Button {...linkProps} icon={ChevronLeft} theme="gray">\n        Go Home\n      </Button>\n    </YStack>\n  );\n} `
+      let screenFileData = new TextEncoder().encode(
+        `import { Paragraph, YStack } from "@my/ui";\nimport React from "react";\n\nexport function ${
+          screenName + "Screen"
+        }() {\n  return (\n    <YStack f={1} jc="center" ai="center" space>\n      <Paragraph ta="center" fow="800">\n        ${screenName.toLowerCase()}\n      </Paragraph>\n    </YStack>\n  );\n} `
       );
+      if (!isStaticRoute) {
+        screenFileData = new TextEncoder().encode(
+          `import { Paragraph, YStack } from "@my/ui";\nimport React from "react";\nimport { createParam } from "solito";\n\nconst { useParam } = createParam<{ ${parameterName!.toLowerCase()}: string }>()\n\nexport function ${
+            screenName + "Screen"
+          }() {\n  const [${parameterName!.toLowerCase()}] = useParam('${parameterName!.toLowerCase()}')\n\n  return (\n    <YStack f={1} jc="center" ai="center" space>\n      <Paragraph ta="center" fow="800">\n        {\`${parameterName!.toLowerCase()}: \${${parameterName!.toLowerCase()}}\`}\n      </Paragraph>\n    </YStack>\n  );\n} `
+        );
+      }
       await vscode.workspace.fs.writeFile(fileUri, screenFileData);
 
-      // Same for navigation/native/index.tsx
-      const navigationFileUri = vscode.Uri.file(
-        path.join(
-          workspaceFolder.uri.fsPath,
-          "packages",
-          "app",
-          "navigation",
-          "native",
-          "index.tsx"
-        )
-      );
+      // Check to see if this is an app created with the expo router template
+      const withExpoRouter = await isWithExpoRouter(workspaceFolder);
 
-      const navigationIndex = await vscode.workspace.openTextDocument(
-        navigationFileUri
-      );
-      const navContents = navigationIndex.getText();
+      if (!withExpoRouter) {
+        // Same for navigation/native/index.tsx
+        const navigationFileUri = vscode.Uri.file(
+          path.join(
+            workspaceFolder.uri.fsPath,
+            "packages",
+            "app",
+            "navigation",
+            "native",
+            "index.tsx"
+          )
+        );
 
-      const newImport = `import { ${screenName} } from "../../features/${screenName.toLowerCase()}/screen";\n`;
-      const startingCreateStack = navContents.lastIndexOf("import");
-      const newRoute = `  ${screenName.toLowerCase()}: undefined;\n`;
-      const closingCreateStack = navContents.indexOf("}>();");
-      const newStackScreen = `  <Stack.Screen\n        name="${screenName.toLowerCase()}"\n        component={${screenName}}\n        options={{\n          title: "${screenName}",\n        }}\n      />\n`;
-      const closingNavIndex = navContents.indexOf("</Stack.Navigator>");
+        const navigationIndex = await vscode.workspace.openTextDocument(
+          navigationFileUri
+        );
+        const navContents = navigationIndex.getText();
 
-      const newNavContents =
-        navContents.substring(0, startingCreateStack) +
-        newImport +
-        navContents.substring(startingCreateStack, closingCreateStack) +
-        newRoute +
-        navContents.substring(closingCreateStack, closingNavIndex) +
-        newStackScreen +
-        navContents.substring(closingNavIndex);
+        const newImport = `import { ${
+          screenName + "Screen"
+        } } from "../../features/${screenName.toLowerCase()}/screen";\n`;
+        const startingCreateStack = navContents.lastIndexOf("import");
+        let newRoute = `  ${screenName.toLowerCase()}: undefined;\n`;
+        if (!isStaticRoute) {
+          newRoute = `  "${screenName.toLowerCase()}": {\n    ${parameterName!.toLowerCase()}:string;\n  };\n`;
+        }
+        const closingCreateStack = navContents.indexOf("}>();");
+        const newStackScreen = `  <Stack.Screen\n        name="${screenName.toLowerCase()}"\n        component={${
+          screenName + "Screen"
+        }}\n        options={{\n          title: "${screenName}",\n        }}\n      />\n`;
+        const closingNavIndex = navContents.indexOf("</Stack.Navigator>");
 
-      // Write the new contents to the navigation file
-      const newNavFileData = new TextEncoder().encode(newNavContents);
-      vscode.workspace.fs.writeFile(navigationFileUri, newNavFileData);
+        const newNavContents =
+          navContents.substring(0, startingCreateStack) +
+          newImport +
+          navContents.substring(startingCreateStack, closingCreateStack) +
+          newRoute +
+          navContents.substring(closingCreateStack, closingNavIndex) +
+          newStackScreen +
+          navContents.substring(closingNavIndex);
 
-      // Same for provider/navigation/index.tsx
-      const navigationProviderFileUri = vscode.Uri.file(
-        path.join(
-          workspaceFolder.uri.fsPath,
-          "packages",
-          "app",
-          "provider",
-          "navigation",
-          "index.tsx"
-        )
-      );
+        // Write the new contents to the navigation file
+        const newNavFileData = new TextEncoder().encode(newNavContents);
+        vscode.workspace.fs.writeFile(navigationFileUri, newNavFileData);
 
-      const navigationProviderIndex = await vscode.workspace.openTextDocument(
-        navigationProviderFileUri
-      );
-      const navProviderContents = navigationProviderIndex.getText();
+        // Same for provider/navigation/index.tsx
+        const navigationProviderFileUri = vscode.Uri.file(
+          path.join(
+            workspaceFolder.uri.fsPath,
+            "packages",
+            "app",
+            "provider",
+            "navigation",
+            "index.tsx"
+          )
+        );
 
-      const newNavRoute = `  ${screenName.toLowerCase()}: "${screenName.toLowerCase()}",\n`;
-      const closingScreenObject = navProviderContents.indexOf(
-        "},",
-        navProviderContents.indexOf("screens: {")
-      );
+        const navigationProviderIndex = await vscode.workspace.openTextDocument(
+          navigationProviderFileUri
+        );
+        const navProviderContents = navigationProviderIndex.getText();
 
-      const newNavProviderContents =
-        navProviderContents.substring(0, closingScreenObject) +
-        newNavRoute +
-        navProviderContents.substring(closingScreenObject);
+        let newNavRoute = `  ${screenName.toLowerCase()}: "${screenName.toLowerCase()}",\n`;
+        if (!isStaticRoute) {
+          newNavRoute = `  ${screenName.toLowerCase()}: "${screenName.toLowerCase()}/:${parameterName!.toLowerCase()}",\n`;
+        }
+        const closingScreenObject = navProviderContents.indexOf(
+          "},",
+          navProviderContents.indexOf("screens: {")
+        );
 
-      // Write the new contents to the navigation file
-      const newNavProviderFileData = new TextEncoder().encode(
-        newNavProviderContents
-      );
-      vscode.workspace.fs.writeFile(
-        navigationProviderFileUri,
-        newNavProviderFileData
-      );
+        const newNavProviderContents =
+          navProviderContents.substring(0, closingScreenObject) +
+          newNavRoute +
+          navProviderContents.substring(closingScreenObject);
 
+        // Write the new contents to the navigation file
+        const newNavProviderFileData = new TextEncoder().encode(
+          newNavProviderContents
+        );
+        vscode.workspace.fs.writeFile(
+          navigationProviderFileUri,
+          newNavProviderFileData
+        );
+      } else {
+        // In the case that this is an expo-router app
+        let expoFolderUri = vscode.Uri.file(
+          path.join(workspaceFolder.uri.fsPath, "apps", "expo", "app")
+        );
+        let expoFileUri = vscode.Uri.file(
+          path.join(expoFolderUri.fsPath, `${screenName.toLowerCase()}.tsx`)
+        );
+
+        if (!isStaticRoute) {
+          expoFolderUri = vscode.Uri.file(
+            path.join(
+              workspaceFolder.uri.fsPath,
+              "apps",
+              "expo",
+              "app",
+              screenName.toLowerCase()
+            )
+          );
+
+          expoFileUri = vscode.Uri.file(
+            path.join(
+              expoFolderUri.fsPath,
+              `[${parameterName!.toLowerCase()}].tsx`
+            )
+          );
+        }
+
+        // Create the new screen folder
+        vscode.workspace.fs.createDirectory(expoFolderUri);
+
+        // Create the new screen file
+        const screenFileData = new TextEncoder().encode(
+          `import { ${
+            screenName + "Screen"
+          } } from "app/features/${screenName.toLowerCase()}/screen";\n\nexport default function () {\n    return <${
+            screenName + "Screen"
+          }/>\n}\n`
+        );
+        await vscode.workspace.fs.writeFile(expoFileUri, screenFileData);
+      }
+
+      // This is common between the two app templates
       // Same for apps/nextjs/pages
       const nextFolderUri = vscode.Uri.file(
         path.join(
@@ -133,19 +242,32 @@ export function activate(context: vscode.ExtensionContext) {
           "apps",
           "nextjs",
           "pages",
-          screenName
+          screenName.toLowerCase()
         )
       );
-      const nextFileUri = vscode.Uri.file(
+      let nextFileUri = vscode.Uri.file(
         path.join(nextFolderUri.fsPath, "index.tsx")
       );
+
+      if (!isStaticRoute) {
+        nextFileUri = vscode.Uri.file(
+          path.join(
+            nextFolderUri.fsPath,
+            `[${parameterName!.toLowerCase()}].tsx`
+          )
+        );
+      }
 
       // Create the new nextjs folder
       vscode.workspace.fs.createDirectory(nextFolderUri);
 
       // Create the new nextjs file
       const nextFileData = new TextEncoder().encode(
-        `import { ${screenName} } from 'app/features/${screenName.toLowerCase()}/screen'\n\nexport default ${screenName}\n`
+        `import { ${
+          screenName + "Screen"
+        } } from 'app/features/${screenName.toLowerCase()}/screen'\n\nexport default ${
+          screenName + "Screen"
+        }\n`
       );
       await vscode.workspace.fs.writeFile(nextFileUri, nextFileData);
 
